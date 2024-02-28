a18800da70e068dd81e71be69728fdbc26adec91
linpack12 <55255203+linpack12@users.noreply.github.com>
2024-02-28 17:49:57 +0100

    player o sånt prefab

--- Lost in Rome/Assets/Scripts/Player/PlayerController.cs
+++ Lost in Rome/Assets/Scripts/Player/PlayerController.cs
@@ -1,36 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 1f;
+    [SerializeField] private float speed = 5f;
+
+    public ContactFilter2D contactFilter;
+    public float collisionOffset = 0.05f;
+
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     private Vector2 moveInput;
     private Rigidbody2D rb;
-    private SpriteRenderer sprite;
     private Animator anim;
+    private SpriteRenderer sprite;
+
+    private Vector2 lastMoveDirection;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+        anim.SetFloat("Horizontal", Mathf.Abs(moveInput.x));
+        anim.SetFloat("Vertical", moveInput.y);
 
-        bool isRunning = moveInput.x != 0;
-        anim.SetBool("IsRunning", isRunning);
-
-        sprite.flipX = moveInput.x < 0;
+        if (moveInput.x != 0)
+        {
+            sprite.flipX = moveInput.x < 0;
+        }
     }
 
-    void OnMove(InputValue value)
+    private void FixedUpdate()
     {
-        moveInput = value.Get<Vector2>();
+        if (moveInput != Vector2.zero && CanMove(moveInput))
+        {
+            rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+        }
     }
+
+    private bool CanMove(Vector2 direction)
+    {
+       if (direction != Vector2.zero) 
+       {
+           int count = rb.Cast(
+               direction, 
+               contactFilter, 
+               castCollisions, 
+               speed * Time.fixedDeltaTime + collisionOffset 
+           );
+
+           if (count == 0) 
+           {
+                return true;
+           }
+       }
+       return false;
+    }
+    public void OnAttack(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            anim.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("IsAttacking", false);
+        }
+    }
+
+    void OnMove(InputValue moveValue) 
+    {
+        moveInput = moveValue.Get<Vector2>();
+    }
+
 }
