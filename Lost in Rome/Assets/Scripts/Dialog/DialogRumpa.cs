2351d84a5cbdbe3035fb12b6cec7654b0548d4c0
linpack12 <55255203+linpack12@users.noreply.github.com>
2024-03-06 15:31:47 +0100

    bajs

--- Lost in Rome/Assets/Scripts/Dialog/DialogRumpa.cs
+++ Lost in Rome/Assets/Scripts/Dialog/DialogRumpa.cs
@@ -9,80 +9,86 @@ public class DialogRumpa : MonoBehaviour
     [SerializeField] GameObject dialogBox;
     [SerializeField] TextMeshProUGUI dialogText;
     [SerializeField] int bokstäverPerSekund;
-
+    
     public static DialogRumpa Instance { get; private set; }
 
+    private Dialog dialog;
+    private int currentRad = 0;
+    private bool isTyping;
     private void Awake()
     {
         Instance = this;
     }
 
-    Dialog dialog;
-    int currentRad = 0;
-    bool isTyping;
-
     public IEnumerator VisaDialog(Dialog dialog)
     {
-        yield return new WaitForEndOfFrame();
-
+        EndDialog();
         this.dialog = dialog;
+        currentRad = 0;
         dialogBox.SetActive(true);
-        StartCoroutine(TypeDialog(dialog.Rader[0]));
+        NextSentence();
+        yield return null;
     }
 
     private void Update()
-    {
-        HandleUpdate();
-        if(Input.GetKeyDown(KeyCode.Escape) && dialogBox.activeInHierarchy)
-        {
-            CancelDialog();
-        }
-    }
-
-    public void HandleUpdate()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (!isTyping)
             {
-                ++currentRad;
-                if (currentRad < dialog.Rader.Count)
-                {
-                    StartCoroutine(TypeDialog(dialog.Rader[currentRad]));
-                }
-                else
-                {
-                    currentRad = 0;
-                    dialogBox.SetActive(false);
-                    PlayerController.Instance.CanMove = true; // Låt spelaren röra sig igen
-                }
+                NextSentence();
             }
             else
             {
-                // Snabba fram till slutet av nuvarande rad om spelaren trycker på "E" under utskrift
-                StopAllCoroutines(); // Stoppar den nuvarande korutinen
-                dialogText.text = dialog.Rader[currentRad]; // Visar hela raden direkt
-                isTyping = false; // Sätter isTyping till false så att nästa rad kan börja
+                CompleteTypingCurrentLine();
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && dialogBox.activeInHierarchy)
+        {
+            EndDialog();
         }
     }
 
-    public void CancelDialog()
+    void NextSentence()
     {
-        StopAllCoroutines();
-        dialogBox.SetActive(false);
-        PlayerController.Instance.CanMove = true;
+        if (currentRad < dialog.Rader.Count)
+        {
+            isTyping = true; 
+            dialogText.text = ""; 
+            StartCoroutine(TypeDialog(dialog.Rader[currentRad]));
+            currentRad++;
+        }
+        else
+        {
+            EndDialog();
+        }
     }
 
-    public IEnumerator TypeDialog(string dialog)
+    private void CompleteTypingCurrentLine()
+    {
+        StopAllCoroutines();
+        isTyping = false;
+        dialogText.text = dialog.Rader[currentRad]; 
+    }
+
+    public IEnumerator TypeDialog(string rad)
     {
         isTyping = true;
         dialogText.text = "";
-        foreach(var bokstav in dialog.ToCharArray())
+        foreach (var letter in rad.ToCharArray())
         {
-            dialogText.text += bokstav;
+            dialogText.text += letter;
             yield return new WaitForSeconds(1f / bokstäverPerSekund);
         }
         isTyping = false;
+    }
+
+    public void EndDialog()
+    {
+        StopAllCoroutines();
+        dialogBox.SetActive(false);
+        currentRad = 0; 
+        isTyping = false; 
+        PlayerController.Instance.CanMove = true; 
     }
 }
