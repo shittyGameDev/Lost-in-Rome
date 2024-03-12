2c908c9b20fe407716e4b86ff4aa3afd2a88da3c
shittyGameDev <victor.berg1234@gmail.com>
2024-03-12 12:40:44 +0100

    Pushing

--- Lost in Rome/Assets/Scripts/NPC/TextBoxTrigger.cs
+++ Lost in Rome/Assets/Scripts/NPC/TextBoxTrigger.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextBoxTrigger : MonoBehaviour
 {
     public GameObject textBox;
+    public TextMeshProUGUI text;
+    public GameObject questText;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && other.GetComponent<QuestManager>().questCompleted == false)
         {
             textBox.SetActive(true);
+            questText.SetActive(true);
+        }
+        else if(other.GetComponent<QuestManager>().questCompleted == true)
+        {
+            text.text = "Thank you! This will help us build bridges and many other things!";
+            textBox.SetActive(true);
+            
         }
     }
 
