d49153b8bb012f5596c435bc3f87326630e3664c
shittyGameDev <victor.berg1234@gmail.com>
2024-03-13 15:12:50 +0100

    Fixes

--- Lost in Rome/Assets/Scripts/NPC/WizeOldMan.cs
+++ Lost in Rome/Assets/Scripts/NPC/WizeOldMan.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class WizeOldMan : MonoBehaviour
 {
-    
-    void Start()
+    public GameManager gameManager;
+    public GameObject canvas;
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            gameManager.AddScore();
+            Debug.Log("Score: " + gameManager.completedLevels);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            canvas.SetActive(true);
+        }
     }
 }
