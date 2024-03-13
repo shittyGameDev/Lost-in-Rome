da57a93145aca7bef056dfc24cffea146128d08b
Charkorz <andres17@live.se>
2024-03-13 14:09:19 +0100

    HubManager

---
+++ Lost in Rome/Assets/SpawnSenateBoi.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSenateBoi : MonoBehaviour
+{
+    public GameObject SenateBoi;
+    void Start()
+    {
+        SenateBoi.SetActive(false);
+    }
+
+    
+}
