c73916f64dba113b5e30bdc2e85392a307a7601d
Charkorz <andres17@live.se>
2024-03-19 13:18:50 +0100

    Fixade lite kommentarer samt endgame skärm

--- Lost in Rome/Assets/Scripts/HubGameManager.cs
+++ Lost in Rome/Assets/Scripts/HubGameManager.cs
@@ -8,13 +8,17 @@ public class HubGameManager : MonoBehaviour
     public GameObject instructionsCanvas;
     void Start()
     {
+        // Sätter på canvasen när scenen startar
         instructionsCanvas.SetActive(true);
+        // Fryser spelaren
         Time.timeScale = 0;
     }
 
     public void startGame()
     {
+        // Stänger av canvasen
         instructionsCanvas.SetActive(false);
+        // Gör att spelaren kan röra sig
         Time.timeScale = 1;
     }
 }
